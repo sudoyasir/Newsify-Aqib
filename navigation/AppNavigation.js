f46c9df1b4152d29ad81d068d30ef090d8a1c3c4
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-11-12 01:04:43 +0500

    :bulb: Proper comments in AppNavigation.js

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,14 +1,19 @@
+import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import NewsFeed from "../component/NewsFeed";
-import SearchNews from "../component/SearchNews";
 import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 import { myTheme } from "./navigationColor";
 import MyStack from "./NewsInfoNavigator";
 import NewsSearchNavigation from "./NewsSearchNavigation";
 import About from "../component/About";
+
+// Create a Bottom Tab Navigator using @react-navigation/bottom-tabs
 const Tab = createBottomTabNavigator();
 
+/**
+ * AppNavigator component represents the main navigation structure of the app.
+ * It includes a Bottom Tab Navigator with three tabs: News Feed, Search News, and About Developer.
+ */
 const AppNavigator = () => {
   return (
     <NavigationContainer theme={myTheme}>
@@ -20,38 +25,44 @@ const AppNavigator = () => {
           tabBarInactiveTintColor: "#262626",
         }}
       >
+        {/* News Feed Tab */}
         <Tab.Screen
+          name="feed"
+          component={MyStack}
           options={{
             title: "News Feed",
             tabBarIcon: ({ size, color }) => (
               <FontAwesome color={color} size={size} name="newspaper-o" />
             ),
           }}
-          name="feed"
-          component={MyStack}
         />
+
+        {/* Search News Tab */}
         <Tab.Screen
+          name="search"
+          component={NewsSearchNavigation}
           options={{
             title: "Search News",
             tabBarIcon: ({ size, color }) => (
               <MaterialCommunityIcons color={color} size={30} name="magnify" />
             ),
           }}
-          name="search"
-          component={NewsSearchNavigation}
         />
+
+        {/* About Developer Tab */}
         <Tab.Screen
+          name="about"
+          component={About}
           options={{
             title: "About Developer",
             tabBarIcon: ({ size, color }) => (
               <MaterialCommunityIcons color={color} size={30} name="face-man" />
             ),
           }}
-          name="about"
-          component={About}
         />
       </Tab.Navigator>
     </NavigationContainer>
   );
 };
+
 export default AppNavigator;
