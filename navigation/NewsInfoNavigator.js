7531cf511582c1ac332afebfa6b14447d72613b8
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-11-12 01:08:53 +0500

    :bulb: Added comments in NewsInfoNavigator.js

--- navigation/NewsInfoNavigator.js
+++ navigation/NewsInfoNavigator.js
@@ -2,21 +2,31 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import NewsFeed from "../component/NewsFeed";
 import NewsInfo from "../component/NewsInfo";
+
+// Create a Stack Navigator using @react-navigation/stack
 const Stack = createStackNavigator();
 
+/**
+ * MyStack component represents a stack-based navigation for the News Feed section.
+ * It includes screens for the News Feed and detailed News Information.
+ */
 function MyStack() {
   return (
     <Stack.Navigator
       screenOptions={{ headerShown: false }}
       initialRouteName="Home"
     >
+      {/* Home Screen (News Feed) */}
       <Stack.Screen name="Home" component={NewsFeed} />
+
+      {/* News Information Screen */}
       <Stack.Screen
-        options={{ presentation: "modal" }}
         name="Info"
         component={NewsInfo}
+        options={{ presentation: "modal" }}
       />
     </Stack.Navigator>
   );
 }
+
 export default MyStack;
