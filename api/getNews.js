bc7347be89466b4326ecf8b40f31859f3d9444ba
Yasir Nawaz <94890956+yasir2002@users.noreply.github.com>
2023-11-25 20:27:04 +0500

    :bulb: added comments

--- api/getNews.js
+++ api/getNews.js
@@ -1,12 +1,18 @@
 import api from "./client";
+
+//api to get news on homescreen
 const getNews = () => {
   return api.get(
     "top-headlines?country=us&apiKey=335a31345a5944eebd407a78852f661e"
   );
 };
+
+//api to get searched news
 const searchedNews = (str) => {
   return api.get(`everything?q=${str}&apiKey=335a31345a5944eebd407a78852f661e`);
 };
+
+//exporting apis
 export default {
   getNews,
   searchedNews,
